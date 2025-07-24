345c3492d777cfdc7e2a6b95ba1669451a63be1e
Harshanandan D <harsha.sachin1234@gmail.com>
2025-07-25 02:43:06 +0530

    Update context_manager.py

--- context_manager.py
+++ context_manager.py
@@ -1,12 +1,1 @@
-class ChatContext:
-    def __init__(self):
-        self.history = []
 
-    def add(self, user_input, bot_response):
-        self.history.append({"user": user_input, "bot": bot_response})
-
-    def get_prompt(self):
-        context = ""
-        for turn in self.history[-5:]:
-            context += f"User: {turn['user']}\nBot: {turn['bot']}\n"
-        return context
