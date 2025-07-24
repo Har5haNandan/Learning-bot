c5dc2cf455dcf992c0363ed4058920f39ce00eaf
Harshanandan D <harsha.sachin1234@gmail.com>
2025-07-25 02:42:50 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,17 +1,1 @@
-from chatbot import get_answer
 
-def run_cli():
-    print("AWS Learning Chatbot | Type 'exit' to quit.\n")
-    while True:
-        query = input("🧑 You: ")
-        if query.lower() in ["exit", "quit"]:
-            break
-        response, sources = get_answer(query)
-        print("\n🤖 Bot:", response)
-        print("\n🔗 Sources:")
-        for src in sources:
-            print(f" - {src['title']}: {src['url']}")
-        print("\n" + "-"*60 + "\n")
-
-if __name__ == "__main__":
-    run_cli()
